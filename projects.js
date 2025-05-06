687001a93d56f639822a5b03d1e23b8b4bf7d49e
Astware Developer <astwareincorporated@gmail.com>
2025-05-06 09:51:48 +0400

    Update projects.js

--- projects.js
+++ projects.js
@@ -9,7 +9,7 @@ const projects_list = [
     {
         title: "Webhook Manager",
         description: "Webhook Manager is a Roblox exploit tool that detects and blocks malicious webhooks—like Discord webhooks used to steal data. It lets you view, block, delete, or spoof webhook requests through a clean, draggable UI.",
-        image: "https://github.com/ScripterTSBG/Webhook-Manager/blob/main/image.png?raw=true",
+        image: "https://github.com/AstwareDev/Webhook-Manager/blob/main/image.png?raw=true",
         url: "https://github.com/ScripterTSBG/Webhook-Manager",
         videos: []
     },
@@ -17,21 +17,21 @@ const projects_list = [
         title: "RuthlessRemotes (Discontinued)",
         description: "RuthlessRemotes is a stealthy Roblox LuaU library designed to enable exploiters to transmit encoded data between alts or other exploiters using animation IDs. It operates without relying on remotes or server-side communication, offering a pure and undetectable method of data transfer.",
         image: "https://github.com/ScripterTSBG/RuthlessRemotes/blob/main/Thumbnail.png?raw=true",
-        url: "https://github.com/ScripterTSBG/RuthlessRemotes",
+        url: "https://github.com/AstwareDev/RuthlessRemotes",
         videos: []
     },
     {
         title: "Notification System",
         description: "The NotificationSystem is a simple and lightweight notification manager for Roblox Exploits that allows you to display notifications on the player's screen. Notifications can be customized with a title, message, and duration, and they will automatically fade out after a set period of time.",
         image: "https://cdn.discordapp.com/attachments/1363200458726052000/1363205833269444699/image.png?ex=681501dd&is=6813b05d&hm=4c7be105ae8a9a81e279f8faf617a5e33c919d1a17d865eff4298e258a05f7b0&",
-        url: "https://github.com/ScripterTSBG/Notification-System",
+        url: "https://github.com/AstwareDev/Notification-System",
         videos: []
     },
     {
         title: "AfterImage Library",
         description: "The Afterimage Library lets you generate real-time neon silhouettes of your character’s movement, perfect for speed effects, teleport visuals, dodges, or pure aesthetic flex. It selectively clones key body parts (head, limbs, torso) into fading ghost parts with customizable color, material, and lifetime — all rendered client-side for full control.",
         image: "https://cdn.discordapp.com/attachments/1363198646102917271/1368133102827995167/fTybaFm.png?ex=68171cbe&is=6815cb3e&hm=c87f1161d2309f2df47b9c578699415dbadeb56fdeac219c163b407473c8713d&",
-        url: "https://github.com/ScripterTSBG/Shadow_Cid/blob/main/AfterImage_Module.lua",
+        url: "https://github.com/AstwareDev/Shadow_Cid/blob/main/AfterImage_Module.lua",
         videos: []
     },
 ];
