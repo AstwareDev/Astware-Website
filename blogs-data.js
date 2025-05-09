7fb0486192e066744e8c32634d161a1bc5261c97
Astware Developer <astwareincorporated@gmail.com>
2025-05-09 12:58:36 +0400

    Create blogs-data.js

---
+++ blogs-data.js
@@ -0,0 +1,6 @@
+const blogData = [
+    {
+        title: 'Delayed SentinelAPI development',
+        content: "Currently we have problems to host our backend, because vercel and other hosting applications give error 404 for no reason."
+    }
+]
