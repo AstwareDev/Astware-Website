abfa6e5b234be0c1d7e731923851a9af513b5863
Astware Developer <astwareincorporated@gmail.com>
2025-05-09 16:05:30 +0400

    Update blogs-data.js

--- blogs-data.js
+++ blogs-data.js
@@ -2,7 +2,7 @@ const blogData = [
     {
         title: 'Delayed website backend development',
         content: "Same reason as the previous blog-post, we hope we will find solution to this problem as soon as possible.",
-        body: "i dont know what to say yet blablablalbalblablalbalbla\n*hmmm\n*hmmmm2\n *Hmmmmmmmmmm"
+        body: "i dont know what to say yet blablablalbalblablalbalbla\n* hmmm\n* hmmmm2\n * Hmmmmmmmmmm"
     },
     {
         title: 'Delayed SentinelAPI development',
