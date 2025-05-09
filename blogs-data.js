4914cd4be787387a6d91cc4320f92898bf586e4c
Astware Developer <astwareincorporated@gmail.com>
2025-05-09 13:48:34 +0400

    Update blogs-data.js

--- blogs-data.js
+++ blogs-data.js
@@ -1,5 +1,9 @@
 const blogData = [
     {
+        title: 'Delayed website backend development',
+        content: "Same reason as the previous blog-post, we hope we will find solution to this problem as soon as possible."
+    },
+    {
         title: 'Delayed SentinelAPI development',
         content: "Currently we have problems to host our backend, because vercel and other hosting applications give error 404 for no reason."
     }
